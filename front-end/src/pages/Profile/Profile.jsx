3d7fb165575cf642804aeb10b3adab672fac5fdd
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-28 12:59:42 -0300

    :sparkles: feat: Implementando página de perfil do usuário logado

---
+++ front-end/src/pages/Profile/Profile.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header/Header';
+import './profile.css';
+
+function Profile () {
+  const [selectedTopic, setSelectedTopic] = useState('all');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Simulação de requisição assíncrona para carregar os posts
+    setTimeout(() => {
+      setPosts([
+        { id: 1, topic: 'frontend', content: 'Meu primeiro post de frontend!' },
+        { id: 2, topic: 'backend', content: 'Meu primeiro post de backend!' },
+        { id: 3, topic: 'fundamentos', content: 'Meu primeiro post de fundamentos!' },
+        { id: 4, topic: 'frontend', content: 'Meu segundo post de frontend!' },
+        { id: 5, topic: 'ciencia', content: 'Meu primeiro post de ciência da computação!' },
+        { id: 6, topic: 'backend', content: 'Meu segundo post de backend!' },
+      ]);
+    }, 1000);
+  }, []);
+
+  const filteredPosts = selectedTopic === 'all'
+    ? posts
+    : posts.filter(post => post.topic === selectedTopic);
+
+  return (
+    <>
+      <Header/>
+    <div className="profile">
+      <div className="profile-header">
+        <div className="profile-photo">
+          {/* foto de Perfil do estudante de acordo com o perfil */}
+          <img
+            src="https://placeimg.com/64/64/people/1"
+            alt="Avatar"
+            className="avatar"
+          />
+        </div>
+        <div className="profile-info">
+          {/* Aqui entram as informações do perfil, como nome, bio etc. */}
+          <h1>John Doe</h1>
+          <p>Estudante Trybe Turma {`20`}</p>
+          <p>Módulo atual {`Backend`}</p>
+        </div>
+      </div>
+      <div className="profile-content">
+        <div className="profile-sidebar">
+          <ul className="topics-list">
+            <li
+              className={selectedTopic === 'all' ? 'active' : ''}
+              onClick={() => setSelectedTopic('all')}
+            >
+              Todos os tópicos
+            </li>
+            <li
+              className={selectedTopic === 'fundamentos' ? 'active' : ''}
+              onClick={() => setSelectedTopic('fundamentos')}
+            >
+              Fundamentos
+            </li>
+            <li
+              className={selectedTopic === 'frontend' ? 'active' : ''}
+              onClick={() => setSelectedTopic('frontend')}
+            >
+              Frontend
+            </li>
+            <li
+              className={selectedTopic === 'backend' ? 'active' : ''}
+              onClick={() => setSelectedTopic('backend')}
+            >
+              Backend
+            </li>
+            <li
+              className={selectedTopic === 'ciencia' ? 'active' : ''}
+              onClick={() => setSelectedTopic('ciencia')}
+            >
+              Ciência da Computação
+            </li>
+          </ul>
+        </div>
+        <div className="profile-posts">
+          {filteredPosts.length === 0 && (
+            <p className="no-posts">Nenhum post encontrado</p>
+          )}
+          {filteredPosts.map(post => (
+            <div className="post" key={post.id}>
+              <p className="post-content">{post.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Profile;
